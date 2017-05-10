5aba484b91a09a3147972e3ff5b694d36a792c60
Artrix <artrix@126.com>
2017-05-10 13:38:54 +0800

    New

--- Main.py
+++ Main.py
@@ -1,25 +1,25 @@
 import folium
 from bike_class.bike_class import Bike
-from mobike_api import get_bikes_square,convert_position
+from mobike_api import get_bikes_square, convert_position, get_bikes_square_multiprocessing
 from kml_operations import create
 
 tileset = r'http://{s}.tile.osm.org/{z}/{x}/{y}.png'
 
 # test location
-start_lat = 22
-start_lon = 113
-all_bike = get_bikes_square(start_lat, start_lon, 0.01, 0.01)
+start_lat = 22.6131442749
+start_lon = 113.8617736101
+all_bike = get_bikes_square_multiprocessing(start_lat, start_lon, 0.008, 0.008)
 
-lats=list()
-lons=list()
-ids=list()
+lats = list()
+lons = list()
+ids = list()
 
 for bike in all_bike:
     lats.append(bike.lat)
     lons.append(bike.lon)
     ids.append(bike.bike_id)
 
-lats,lons=convert_position(lats,lons)
+lats, lons = convert_position(lats, lons)
 
 
 map_osm = folium.Map(
@@ -34,6 +34,7 @@ print(len(lons))
 print(len(ids))
 
 for index in range(len(lats)):
-    folium.Marker([lats[index], lons[index]], popup="id=%s"%ids[index]).add_to(map_osm)
+    folium.Marker([start_lat, start_lon], popup="id=%s" %
+                  ids[index]).add_to(map_osm)
 
-# map_osm.save('osm.html')
+map_osm.save('osm.html')
--- mobike_api.py
+++ mobike_api.py
@@ -3,16 +3,20 @@ import json
 from bike_class.bike_class import Bike
 import time
 import requests
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
-def convert_position(lat, lon):
+def convert_position(lat, lon):  # lat,lon are lists.
+
+    print("_______________Point convert Start._______________")
 
     api = "http://api.gpsspg.com/convert/coord/?oid=4888&key=E9088432D4E3924F6A16CB9727E78003&from=3&to=0&latlng="
+    need_request=list()
 
     k = 0
 
-    conv_lats=list()
-    conv_lons=list()
+    conv_lats = list()
+    conv_lons = list()
     for index in range(len(lat)):
         api += str(lat[index])
         api += ","
@@ -20,29 +24,31 @@ def convert_position(lat, lon):
         api += ";"
         if index >= k * 14 + 14:
             k += 1
-            import json
-            response = requests.get(api).text
-            j=json.loads(response)
-            for p in j["result"]:
-                conv_lats.append(p["lat"])
-                conv_lons.append(p["lng"])
+            need_request.append(api)
             api = "http://api.gpsspg.com/convert/coord/?oid=4888&key=E9088432D4E3924F6A16CB9727E78003&from=3&to=0&latlng="
 
     # Get the rest positions
     api = "http://api.gpsspg.com/convert/coord/?oid=4888&key=E9088432D4E3924F6A16CB9727E78003&from=3&to=0&latlng="
-    for index in range(k * 14+1, len(lat)):
+    for index in range(k * 14 + 1, len(lat)):
         api += str(lat[index])
         api += ","
         api += str(lon[index])
         api += ";"
-    import json
-    response = requests.get(api).text
-    j = json.loads(response)
-    for p in j["result"]:
-        conv_lats.append(p["lat"])
-        conv_lons.append(p["lng"])
+    need_request.append(api)
 
-    return conv_lats,conv_lons
+    def request_posision(url):
+        import json
+        response = requests.get(url).text
+        j = json.loads(response)
+        for p in j["result"]:
+            conv_lats.append(p["lat"])
+            conv_lons.append(p["lng"])
+
+    with ThreadPoolExecutor(max_workers=8) as executor:
+        future_process = {executor.submit(request_posision, url): url for url in need_request}
+
+    print("_______________Point convert OK._______________")
+    return conv_lats, conv_lons
 
 
 def get_bikes(lat, lon):
@@ -52,12 +58,13 @@ def get_bikes(lat, lon):
             headers = {
                 'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Mobile/14E304 MicroMessenger/6.5.7 NetType/WIFI Language/zh_CN',
                 'Content-Type': 'application/x-www-form-urlencoded',
-                'Referer': 'https://servicewechat.com/wx80f809371ae33eda/23/page-frame.html',
+                'Referer': 'https://servicewechat.com/wx80f809371ae33eda/26/',
             }
             url = 'https://mwx.mobike.com/mobike-api/rent/nearbyBikesInfo.do'
             data = {
                 'longitude': lon,  # 经度
                 'latitude': lat,  # 纬度
+                'citycode': '0755',
                 'errMsg': 'getMapCenterLocation:ok'
             }
 
@@ -81,8 +88,37 @@ def get_bikes(lat, lon):
             return all_bikes
 
         except:
+            print("retry.")
             continue
         break
+
+def conv(point):
+    lon,lat=point[0],point[1]
+    bikes=get_bikes(lat,lon)
+    return bikes
+
+
+def get_bikes_square_multiprocessing(middle_lat, middle_lon, width, height):
+    start_x = -width / 2 + middle_lon
+    start_y = -height / 2 + middle_lat
+    step = 0.001
+
+    all_bikes = list()
+    all_point = list()
+    for index_x in range(int(width / step)):
+        for index_y in range(int(height / step)):
+            point = [index_x * step + start_x, index_y * step + start_y]
+            all_point.append(point)
+
+    with ThreadPoolExecutor(max_workers=8) as executor:
+        future_process = {executor.submit(conv, p): p for p in all_point}
+        for future in as_completed(future_process):
+            result=future.result()
+            if isinstance(result,list):
+                for bike in result:
+                    all_bikes.append(bike)
+
+    return all_bikes
 
 
 def get_bikes_square(middle_lat, middle_lon, width, height):
